52b920f58a25f66c67aa1c92af0b5ade2577982a
masp005 <spelsberg.marcel@gmail.com>
2025-05-11 02:59:48 +0200

    feat(mod): Change control panel look

--- src/shared.cpp
+++ src/shared.cpp
@@ -44,7 +44,6 @@ CCNode* DeathLocationMin::createAnimatedNode(
 
 CCNode* DeathLocationMin::createNode(bool isCurrent, bool preAnim) const {
 	auto sprite = CCSprite::create("death-marker.png"_spr);
-	std::string const id = "marker"_spr;
 	float markerScale = Mod::get()->getSettingValue<float>("marker-scale");
 	if (isCurrent) {
 		sprite->setScale(markerScale * 1.5f);
