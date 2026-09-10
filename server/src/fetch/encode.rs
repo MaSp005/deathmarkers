648ef0cee22ed42c173be6776662937a6f1411c7
Masp <hi@masp005.dev>
2026-09-10 15:42:29 +0200

    feat(server): add functions to re-encode bytes to csv

--- server/src/fetch/encode.rs
+++ server/src/fetch/encode.rs
@@ -1,13 +1,18 @@
-use crate::digest::{Sha1Result, sha1_digest};
-use bytes::{BufMut, Bytes, BytesMut};
-use hex::FromHex;
+use crate::digest::{SHA1_LENGTH, Sha1Result, sha1_digest};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use hex::{FromHex, ToHex};
 use sqlx::{Row, postgres::PgRow};
 
 pub(super) const DATA_FORMAT: u8 = 1;
+const ITEM_LENGTH_NORMAL: usize = 4 + 4 + 2;
+const ITEM_LENGTH_PLATFORMER: usize = 4 + 4;
+const ITEM_LENGTH_ANALYSIS: usize = 20 + 1 + 1 + 4 + 4 + 2;
+const COLUMNS_NORMAL: &str = "x,y,percentage";
+const COLUMNS_PLATFORMER: &str = "x,y";
+const COLUMNS_ANALYSIS: &str = "userident,levelversion,practice,x,y,percentage";
 
 pub(super) fn create_binary_response_normal(deaths: &Vec<PgRow>) -> Bytes {
-    const ITEM_LENGTH: usize = 4 + 4 + 2;
-    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH + 1);
+    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH_NORMAL + 1);
     bytes.put_u8(DATA_FORMAT);
     for death in deaths {
         bytes.put_f32_le(death.get::<f64, _>(0) as _); // x
@@ -18,8 +23,7 @@ pub(super) fn create_binary_response_normal(deaths: &Vec<PgRow>) -> Bytes {
 }
 
 pub(super) fn create_binary_response_platformer(deaths: &Vec<PgRow>) -> Bytes {
-    const ITEM_LENGTH: usize = 4 + 4;
-    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH + 1);
+    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH_PLATFORMER + 1);
     bytes.put_u8(DATA_FORMAT);
     for death in deaths {
         bytes.put_f32_le(death.get::<f64, _>(0) as _); // x
@@ -29,9 +33,7 @@ pub(super) fn create_binary_response_platformer(deaths: &Vec<PgRow>) -> Bytes {
 }
 
 pub(super) fn create_binary_response_analysis(deaths: &Vec<PgRow>, salt: Option<&str>) -> Bytes {
-    const ITEM_LENGTH: usize = 20 + 1 + 1 + 4 + 4 + 2;
-
-    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH + 1);
+    let mut bytes = BytesMut::with_capacity(deaths.len() * ITEM_LENGTH_ANALYSIS + 1);
     bytes.put_u8(DATA_FORMAT);
     for death in deaths {
         let userident: String = death.get(0);
@@ -50,10 +52,58 @@ pub(super) fn create_binary_response_analysis(deaths: &Vec<PgRow>, salt: Option<
     bytes.freeze()
 }
 
+pub fn transform_to_csv_normal(mut bytes: Bytes) -> String {
+    assert_eq!(bytes.len() % ITEM_LENGTH_NORMAL, 1);
+    let mut lines: Vec<String> = Vec::with_capacity(bytes.len() / ITEM_LENGTH_NORMAL + 1);
+    lines.push(COLUMNS_NORMAL.to_owned());
+    bytes.get_u8();
+    while bytes.remaining() >= ITEM_LENGTH_NORMAL {
+        let x = bytes.get_f32_le();
+        let y = bytes.get_f32_le();
+        let percentage = bytes.get_u16_le();
+        lines.push(format!("{x},{y},{percentage}"));
+    }
+    lines.join("\n")
+}
+
+pub fn transform_to_csv_platformer(mut bytes: Bytes) -> String {
+    assert_eq!(bytes.len() % ITEM_LENGTH_PLATFORMER, 1);
+    let mut lines: Vec<String> = Vec::with_capacity(bytes.len() / ITEM_LENGTH_PLATFORMER + 1);
+    lines.push(COLUMNS_PLATFORMER.to_owned());
+    bytes.get_u8();
+    while bytes.remaining() >= ITEM_LENGTH_PLATFORMER {
+        let x = bytes.get_f32_le();
+        let y = bytes.get_f32_le();
+        lines.push(format!("{x},{y}"));
+    }
+    lines.join("\n")
+}
+
+pub fn transform_to_csv_analysis(mut bytes: Bytes) -> String {
+    assert_eq!(bytes.len() % ITEM_LENGTH_ANALYSIS, 1);
+    let mut lines: Vec<String> = Vec::with_capacity(bytes.len() / ITEM_LENGTH_ANALYSIS + 1);
+    lines.push(COLUMNS_ANALYSIS.to_owned());
+    bytes.get_u8();
+    while bytes.remaining() >= ITEM_LENGTH_ANALYSIS {
+        let ui = bytes.split_to(SHA1_LENGTH);
+        let ui: String = ui.encode_hex();
+        let version = bytes.get_u8();
+        let practice = bytes.get_u8();
+        assert!(practice < 2);
+        let practice = practice == 1;
+        let x = bytes.get_f32_le();
+        let y = bytes.get_f32_le();
+        let percentage = bytes.get_u16_le();
+        lines.push(format!("{ui},{version},{practice},{x},{y},{percentage}"));
+    }
+    lines.join("\n")
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     use crate::digest::Sha1Result;
+    use bytes::Bytes;
     use hex::FromHex;
     use sqlx::query;
 
@@ -63,12 +113,18 @@ mod test {
         let bytes = create_binary_response_normal(&deaths);
         assert_eq!(bytes.len(), 1);
         assert_eq!(bytes, Bytes::from(vec![DATA_FORMAT]));
+        let csv = transform_to_csv_normal(bytes);
+        assert_eq!(csv, COLUMNS_NORMAL);
         let bytes = create_binary_response_platformer(&deaths);
         assert_eq!(bytes.len(), 1);
         assert_eq!(bytes, Bytes::from(vec![DATA_FORMAT]));
+        let csv = transform_to_csv_platformer(bytes);
+        assert_eq!(csv, COLUMNS_PLATFORMER);
         let bytes = create_binary_response_analysis(&deaths, None);
         assert_eq!(bytes.len(), 1);
         assert_eq!(bytes, Bytes::from(vec![DATA_FORMAT]));
+        let csv = transform_to_csv_analysis(bytes);
+        assert_eq!(csv, COLUMNS_ANALYSIS);
     }
 
     #[sqlx::test]
@@ -99,6 +155,10 @@ mod test {
             u16::to_le_bytes(1),
             "percentage as LE uint16"
         );
+
+        let csv = transform_to_csv_normal(bytes);
+        println!("{csv}");
+        assert_eq!(csv, format!("{COLUMNS_NORMAL}\n5.3,9.4,1"));
     }
 
     #[sqlx::test]
@@ -124,6 +184,10 @@ mod test {
             f32::to_le_bytes(9.4),
             "y as LE float32"
         );
+
+        let csv = transform_to_csv_platformer(bytes);
+        println!("{csv}");
+        assert_eq!(csv, format!("{COLUMNS_PLATFORMER}\n5.3,9.4"));
     }
 
     #[sqlx::test]
@@ -167,6 +231,15 @@ mod test {
             death_slice[0x1e..=0x1f],
             u16::to_le_bytes(1),
             "percentage as LE uint16"
+        );
+
+        let csv = transform_to_csv_analysis(bytes);
+        println!("{csv}");
+        assert_eq!(
+            csv,
+            format!(
+                "{COLUMNS_ANALYSIS}\n0123456789abcdef0123456789abcdef01234567,42,true,5.3,9.4,1"
+            )
         );
     }
 }
